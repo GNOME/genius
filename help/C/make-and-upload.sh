1a28573cb6b298552862b9e50dde25c6fd55cbf4
Jiri (George) Lebl <jirka@5z.com>
2010-09-09 23:25:39 -0700

    relase 1.0.11 and other minor tidbits
    Thu Sep 09 22:47:54 2010  Jiri (George) Lebl <jirka@5z.com>
    
    	* Release 1.0.11
    
    	* src/gnome-genius.desktop.in: add %U to command line
    
    	* help/C/genius.xml: some minor updates and clarifications

--- help/C/make-and-upload.sh
+++ help/C/make-and-upload.sh
@@ -4,16 +4,18 @@ if [ ! -d /home/jirka/ ]; then
 	exit
 fi
 
-echo rm -f *.html *.pdf
-rm -f *.html *.pdf
+echo rm -f *.html *.pdf *.ps
+rm -f *.html *.pdf *.ps
 
 echo SP_ENCODING=\"utf-8\" docbook2html genius.xml
 SP_ENCODING="utf-8" docbook2html genius.xml
 
-echo docbook2ps genius.xml
-docbook2ps genius.xml
-echo ps2pdf genius.ps genius.pdf
-ps2pdf genius.ps genius.pdf
+echo docbook2pdf genius.xml
+docbook2pdf genius.xml
+#echo docbook2ps genius.xml
+#docbook2ps genius.xml
+#echo ps2pdf genius.ps genius.pdf
+#ps2pdf genius.ps genius.pdf
 
 echo scp *.html zinc.5z.com:/home/www/html/jirka/genius-documentation/
 scp *.html zinc.5z.com:/home/www/html/jirka/genius-documentation/
