a453eb3e05b8b9c398b3e09c5aa3aa18e8e385b8
Jiri (George) Lebl <jiri.lebl@gmail.com>
2017-04-29 16:58:27 -0500

    Sat Apr 29 16:58:25 2017  Jiri (George) Lebl <jirka@5z.com>
    	* help/make-makefile-am.sh, help/update-xml-to-txt-html.sh, */*.html,
    	  Makefile.am:  Since automake is smarter than I am and knows how I
    	  should work, I made a script to generate a Makefile.am so that it
    	  can't complain.  Now runs through distcheck with no errors!

--- help/update-xml-to-txt-html.sh
+++ help/update-xml-to-txt-html.sh
@@ -12,6 +12,14 @@ LANGS="cs de el es fr pt_BR ru sv"
 for n in $LANGS ; do
 	echo Running xml2po -e -p $n/$n.po -o $n/genius.xml C/genius.xml
 	xml2po -e -p $n/$n.po -o $n/genius.xml C/genius.xml || exit 1
+	echo Running rm -f $n/html/*.html
+	rm -f $n/html/*.html || exit 1
 	echo Running xmlto -o $n/html/ html $n/genius.xml
 	xmlto -o $n/html/ html $n/genius.xml || exit 1
 done
+
+echo Running make-makefile-am.sh
+./make-makefile-am.sh
+
+echo
+echo Now you should rerun automake I suppose ...
