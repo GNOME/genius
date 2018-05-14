750caea342a2b8bed297fb335372c10f34f36b6c
Jiri (George) Lebl <jiri.lebl@gmail.com>
2018-05-14 16:45:35 -0500

    minor updates, add my script to make docs

---
+++ help/C/make-only.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+# This script is for George only, so recognize my machine :)
+if [ ! -d /home/jirka/ ]; then
+	echo "Only George wants to run this script (it's for updating the genius webpage)"
+	exit
+fi
+
+echo rm -f *.html *.pdf *.ps
+rm -f *.html *.pdf *.ps
+
+#echo SP_ENCODING=\"utf-8\" docbook2html genius.xml
+#SP_ENCODING="utf-8" docbook2html genius.xml
+
+echo xmlto html genius.xml
+xmlto html genius.xml
+
+#echo xmlto pdf genius.xml
+#xmlto pdf genius.xml
+
+#echo docbook2pdf genius.xml
+#docbook2pdf genius.xml
+echo dblatex genius.xml
+dblatex genius.xml
+#echo docbook2ps genius.xml
+#docbook2ps genius.xml
+#echo ps2pdf genius.ps genius.pdf
+#ps2pdf genius.ps genius.pdf
+
+#echo scp *.html zinc.5z.com:/home/www/html/jirka/genius-documentation/
+#scp *.html zinc.5z.com:/home/www/html/jirka/genius-documentation/
+
+#echo scp figures/*.png zinc.5z.com:/home/www/html/jirka/genius-documentation/figures/
+#scp figures/*.png zinc.5z.com:/home/www/html/jirka/genius-documentation/figures/
+
+#echo scp genius.pdf zinc.5z.com:/home/www/html/jirka/genius-reference.pdf
+#scp genius.pdf zinc.5z.com:/home/www/html/jirka/genius-reference.pdf
