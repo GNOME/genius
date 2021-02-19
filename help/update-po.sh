8af21a05ba2ffd4a2c2f4c92f4fe210c6ae5b393
Jiri (George) Lebl <jiri.lebl@gmail.com>
2021-02-18 17:20:41 -0800

    Thu Feb 18 17:20:08 2021  Jiri (George) Lebl <jirka@5z.com>
    	* help/update-po.sh,help/update-xml-to-txt-html.sh: port from
    	  xml2po to itstool, and run the updates

--- help/update-po.sh
+++ help/update-po.sh
@@ -3,5 +3,22 @@ if test "x$1" = "x" ; then
 	echo USAGE: ./update-po.sh LANG
 	exit 1
 fi 
-echo Running xml2po -u $1/$1.po C/genius.xml
-xml2po -u $1/$1.po C/genius.xml
+LL="$1"
+#echo Running xml2po -u $1/$1.po C/genius.xml
+#xml2po -u $1/$1.po C/genius.xml
+
+echo
+echo "Running: itstool -o genius.pot C/genius.xml"
+itstool -o genius.pot C/genius.xml || exit 1
+
+echo
+echo "Running: msgmerge -o $LL/new.po $LL/$LL.po genius.pot"
+msgmerge -o $LL/new.po $LL/$LL.po genius.pot || exit 1
+
+echo
+echo "Running: mv -f $LL/new.po $LL/$LL.po"
+mv -f $LL/new.po $LL/$LL.po || exit 1
+
+echo
+echo "Running: rm -f genius.pot"
+rm -f genius.pot
--- help/update-xml-to-txt-html.sh
+++ help/update-xml-to-txt-html.sh
@@ -1,24 +1,58 @@
 #!/bin/sh
 LANGS="cs de el es fr pt_BR ru sv"
 
-echo Running xmlto -o C/html/ html C/genius.xml
+echo
+echo Running: xmlto -o C/html/ html C/genius.xml
 xmlto -o C/html/ html C/genius.xml || exit 1
 
-echo Running docbook2txt C/genius.xml
+echo
+echo Running: docbook2txt C/genius.xml
 docbook2txt C/genius.xml || exit 1
-echo Running dos2unix genius.txt
+
+echo
+echo Running: dos2unix genius.txt
 dos2unix genius.txt || exit 1
 
+echo
+echo "Running: itstool -o genius.pot C/genius.xml"
+itstool -o genius.pot C/genius.xml || exit 1
+
 for n in $LANGS ; do
-	echo Running xml2po -e -p $n/$n.po -o $n/genius.xml C/genius.xml
-	xml2po -e -p $n/$n.po -o $n/genius.xml C/genius.xml || exit 1
+	echo
+	echo "Running: msgmerge -o $n/new.po $n/$n.po genius.pot"
+	msgmerge -o $n/new.po $n/$n.po genius.pot || exit 1
+
+	echo
+	echo "Running: mv -f $n/new.po $n/$n.po"
+	mv -f $n/new.po $n/$n.po || exit 1
+
+	echo
+	echo "Running: msgfmt -o messages.mo $n/$n.po"
+	msgfmt -o messages.mo $n/$n.po || exit 1
+
+	echo
+	echo "Running: itstool -m messages.mo -o $n/ C/genius.xml"
+	itstool -m messages.mo -o $n/ C/genius.xml || exit 1
+
+	echo
+	echo "Running: rm -f messages.mo"
+	rm -f messages.mo
+
+	echo
 	echo Running rm -f $n/html/*.html
 	rm -f $n/html/*.html || exit 1
+
+	echo
 	echo Running xmlto -o $n/html/ html $n/genius.xml
 	xmlto -o $n/html/ html $n/genius.xml || exit 1
 done
 
-echo Running make-makefile-am.sh
+echo
+echo "Running: rm -f messages.mo genius.pot"
+rm -f messages.mo genius.pot
+
+echo
+echo Running: make-makefile-am.sh
 ./make-makefile-am.sh
 
 echo
